89f7da5bd531cc539d0acd537437f7103954eb26
Carlos Araiza <eliptikman1234@gmail.com>
2024-07-04 21:10:16 -0600

    chore: Update SideBar component to use lucide-react-native icons and improve styling

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -1,17 +1,47 @@
-import { View, Text } from "react-native"
+import { Compass, TowerControl, Map } from "lucide-react-native";
+import { View, Text, TouchableOpacity } from "react-native"
 
-type Props = {}
+type Props = {
+    path: string;
+}
 
 type ItemType = {
     children: any;
 }
 
 export const SideBar = (props: Props) => {
+    const { path } = props;
     return (
-        <View className="bg-slate-500">
-            <Text className="text-white font-medium text-sm">
-                Home
-            </Text>
+        <View className={"bg-[#102C57] pt-2"}>
+            <MenuItem active={path == 'home'}>
+                <TowerControl size={20} color={'white'} />
+                <Text className={"text-white font-medium text-[11px] text-center"}>
+                    Airport
+                </Text>
+            </MenuItem>
+            <MenuItem active={path == 'planner'}>
+                <Compass size={20} color={'white'} />
+                <Text className={"text-white font-medium text-[11px] text-center"}>
+                    Planner
+                </Text>
+            </MenuItem>
+            <MenuItem active={path == 'map'}>
+                <Map size={20} color={'white'} />
+                <Text className={"text-white font-medium text-[11px] text-center"}>
+                    Map
+                </Text>
+            </MenuItem>
         </View>
     )
 }
+
+
+const MenuItem = ({ children, active }: { children: any, active?: boolean }) => {
+    return (
+        <TouchableOpacity
+            className={`flex items-center p-2 gap-1 hover:bg-slate-500/10 ${active && 'bg-[#0a1d39]'}`}>
+            {children}
+        </TouchableOpacity
+        >
+    )
+} 
--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, Text, View } from 'react-native';
+import { SafeAreaView, Text, View, TouchableOpacity } from 'react-native';
 import '../styles.css';
-import { Compass, Map } from 'lucide-react-native';
+import { Compass, Map, TowerControl } from 'lucide-react-native';
+import { SideBar } from './components/SideBar';
 // import { SideBar } from './components/SideBar';
 
 export default function App() {
@@ -9,21 +10,17 @@ export default function App() {
     <>
       <SafeAreaView>
         <View className={'flex flex-row w-screen h-screen'}>
-          <View className={"bg-slate-500 h-screen"}>
-            <MenuItem>
-              <Compass size={30} color={'white'} />
-              <Text className={"text-white font-medium text-sm text-center"}>
-                Planner
-              </Text>
-            </MenuItem>
-            <MenuItem>
-              <Map size={30} color={'white'} />
-              <Text className={"text-white font-medium text-sm text-center"}>
-                Map
-              </Text>
-            </MenuItem>
-          </View>
-          <View>
+          {/* Sidebar section */}
+          <SideBar path="home"/>
+          <View className='flex-row items-start'>
+          
+            {/* Description section */}
+            <View className='h-screen bg-slate-400'>
+              <View className='flex flex-row justify-between p-4'>
+                <Text className='font-semibold'>Search airport</Text>
+              </View>
+            </View>
+
             <Text className={"text-4xl font-semibold"} >
               Dashboard Flight Report
             </Text>
@@ -33,12 +30,3 @@ export default function App() {
     </>
   );
 }
-
-
-const MenuItem = ({children}) => {
-  return (
-    <View className='flex items-center'>
-      {children}
-    </View>
-  )
-} 
